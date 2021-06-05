cf64797b10e8d5796f9d46fa24dede4f29f9b750
Gleb <demonxklimenko@mail.ru>
2021-06-05 21:06:30 +0300

    Initial commit

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,16 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] search;
+
+    public Post[] search() {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete() {
+        // TODO: add logic
+    }
+}
