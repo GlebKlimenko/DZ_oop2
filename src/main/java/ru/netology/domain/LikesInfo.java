4398f86c37e68e98efa10e7d3b934d822caffc99
Gleb <demonxklimenko@mail.ru>
2021-06-05 20:07:01 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/LikesInfo.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+
+public class LikesInfo {
+    private int likesCount;
+    private boolean canLike;
+    private boolean isLike;
+}
---
+++ src/main/java/ru/netology/domain/RepostsInfo.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class RepostsInfo {
+    private int repostsCount;
+    private boolean canRepost;
+    private boolean isRepost;
+}
+
